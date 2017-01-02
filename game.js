d09c7dc262719dcbf9782b1f5251100b05ec258c
vetaylor <taylor.victoriae@gmail.com>
2017-01-01 21:32:11 -0600

    My queue popped

---
+++ game.js
@@ -0,0 +1,24 @@
+!function($){
+
+	var defaults = {
+		rows: 9,
+		columns: 9,
+		numberOfBombs: 10
+	};
+
+	var Cell = function Cell(x, y, isBomb) {
+		this.x = x;
+		this.y = y;
+		this.isBomb = isBomb;
+
+		this.activeNeighbors = 0;
+		this.neighbors = [];
+		this.revealed = false;
+		this.mark = "";
+		this.id = y + "-" + x;
+	};
+
+	Cell.prototype.setValue = function(value) {
+		var field =
+	};
+}
