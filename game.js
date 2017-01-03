9b70845e2c44e89e6df5e66e52c511f07ffb5b89
vetaylor <taylor.victoriae@gmail.com>
2017-01-02 20:41:41 -0600

    Added controls

--- game.js
+++ game.js
@@ -1,4 +1,20 @@
-$(document).ready(function ()
-{
-	$('#game').minesweeper();
-});
+var Game = function (rows, columns, numberOfBombs) {
+	this.rows = Number(rows);
+	this.columns = Number(columns);
+	this.numberOfBombs = Number(numberOfBombs);
+	this.board = document.getElementById('board');
+
+	this.loadBoard();
+};
+
+Game.prototype.loadBoard = function () {
+	for(var i = 0; i < rows; i++) {
+		var row = document.createElement('div');
+		for(var j = 0; j < columns; j++) {
+			var cell = document.createElement('button');
+			row.appendChild(cell);
+		}
+
+		board.appendChild(row);
+	}
+};
