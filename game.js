18e4d3416b20e64073e0e67abece8ebf1fae0709
vetaylor <taylor.victoriae@gmail.com>
2017-01-04 20:23:31 -0600

    Added function declarations

--- game.js
+++ game.js
@@ -3,18 +3,30 @@ var Game = function(rows, columns, numberOfBombs) {
 	this.columns = Number(columns);
 	this.numberOfBombs = Number(numberOfBombs);
 	this.board = document.getElementById('board');
+	this.bombCount = document.getElementById('bomb-count');
 
-	this.loadBoard();
+	this.bombCount.innerText = this.numberOfBombs;
+	this.initBoard();
 };
 
-Game.prototype.loadBoard = function() {
+Game.prototype.initBoard = function() {
+	var grid = this.bomb_matrix();
+
 	for(var i = 0; i < this.rows; i++) {
 		var row = document.createElement('div');
+
 		for(var j = 0; j < this.columns; j++) {
 			var cell = document.createElement('button');
 			row.appendChild(cell);
 		}
-
 		this.board.appendChild(row);
-	}
+	}	
+};
+
+Game.prototype.bomb_matrix = function() {
+
+};
+
+Game.prototype.generateBombs = function() {
+
 };
